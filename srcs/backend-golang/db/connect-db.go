43068cf677ecdcfab60b4e2bd87033cbdf586c41
Yoshinari <110902237+deerman31@users.noreply.github.com>
2024-10-15 19:04:46 +0900

    db_connect()をディレクトリに分割

---
+++ srcs/backend-golang/db/connect-db.go
@@ -0,0 +1,36 @@
+package db
+
+import (
+    "database/sql"
+	"fmt"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+
+func ConnectDB() (*sql.DB, error) {
+    // 環境変数から接続情報を取得
+    dbUser := os.Getenv("MYSQL_USER")
+    dbPass := os.Getenv("MYSQL_PASSWORD")
+    dbHost := "db-mysql" // Docker Composeのサービス名
+    dbPort := os.Getenv("MYSQL_PORT")
+    dbName := os.Getenv("MYSQL_DATABASE")
+
+    // データベース接続文字列を構築
+    dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", dbUser, dbPass, dbHost, dbPort, dbName)
+
+    // データベースに接続
+    db, err := sql.Open("mysql", dsn)
+    if err != nil {
+        return nil, fmt.Errorf("failed to connect to database: %v", err)
+    }
+
+    // 接続テスト
+    if err := db.Ping(); err != nil {
+        db.Close()
+        return nil, fmt.Errorf("failed to ping database: %v", err)
+    }
+
+    return db, nil
+}
